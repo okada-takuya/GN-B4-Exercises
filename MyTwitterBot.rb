40df0ddc999e5973b12f0d07fb91608099961ce7
okada <okada@kawasemi2.swlab.cs.okayama-u.ac.jp>
2013-04-04 15:39:36 +0900

    Separate personal information from source code

--- MyTwitterBot.rb
+++ MyTwitterBot.rb
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 require './TwitterBot.rb' # TwitterBot.rbの読み込み
 require 'yaml'
+require 'json'
 
 #---------- MyTwitterBot ----------                                                                         
 class MyTwitterBot < TwitterBot
+
+  def initialize
+    super
+    self.get_bot_id
+  end
 
   # 機能を追加
   def tweet( message )
     message << " tweet by bot."
     super
   end
-  
-  #--------- Okada_Tkyへのリプライを取得  ---------
+
+  #--------- BotのIDを取得する ---------
+  def get_bot_id
+    response = @access_token.get(
+      '/account/verify_credentials.json'
+    )
+    
+    mystatus = JSON.parse( response.body )
+    @bot_id = mystatus["screen_name"]
+
+  end
+
+  #--------- Botへのリプライか真偽を取得  ---------
   def is_reply( tw )
-    return self.search_tweet("@Okada_Tky", tw)
+    return self.search_tweet( @bot_id, tw )
   end
   
   #--------- tweetからstrの存在を検索 ---------
@@ -28,9 +45,9 @@ class MyTwitterBot < TwitterBot
     tweets.each do |post|
       if self.is_reply(post) then
         if self.search_tweet("｣と言って", post) then
-          @head = post["message"].index("｢") + 1
-          @tail = post["message"].rindex("｣") - 1
-          self.tweet( tw["message"].slice(@head..@tail))
+          head = post["message"].index("｢") + 1
+          tail = post["message"].rindex("｣") - 1
+          self.tweet( post["message"].slice(head..tail))
         end
 
       end
@@ -43,17 +60,17 @@ class MyTwitterBot < TwitterBot
   def notice_birth
     birthday_data = YAML.load_file('./birthday.yml')
 
-    @today = Time.now
+    today = Time.now
 
     birthday_data.each do |prof|
-      if prof["month"] == @today.month then
-        if prof["day"] == @today.day then
-          @str = "今日は" 
-          @str << prof["name"] 
-          @str << " @" 
-          @str << prof["ID"] 
-          @str << " さんの誕生日です！みんなでお祝いしましょう！" 
-          self.tweet( @str )
+      if prof["month"] == today.month then
+        if prof["day"] == today.day then
+          str = "今日は" 
+          str << prof["name"] 
+          str << " @" 
+          str << prof["ID"] 
+          str << " さんの誕生日です！みんなでお祝いしましょう！" 
+          self.tweet( str )
         end
 
       end
@@ -70,6 +87,6 @@ end
 print "Start MyTwitterBot.\n"
 mbot = MyTwitterBot.new()
 #mbot.answer_say
-mbot.notice_birth
+#mbot.notice_birth
 print "End MyTwitterBot.\n"
 
