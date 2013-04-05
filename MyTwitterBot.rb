d5a92ecbdebe1d47a4b1a6ac147b8f4c5477e88c
okada <okada@kawasemi2.swlab.cs.okayama-u.ac.jp>
2013-04-05 13:09:14 +0900

    Use a regular expression to search

--- MyTwitterBot.rb
+++ MyTwitterBot.rb
@@ -5,29 +5,31 @@ require 'json'
 
 #---------- MyTwitterBot ----------                                                                         
 class MyTwitterBot < TwitterBot
-
+  
   def initialize
     super
     self.get_bot_id
   end
-
-  # 機能を追加
+  
+  #--------- 以前と同じ内容にならないよう時間を付け加える ---------
   def tweet( message )
-    message << " tweet by bot."
+    sleep(2)
+    time = Time.now
+    message << " tweet by bot." + time.to_s
     super
   end
-
+  
   #--------- BotのIDを取得する ---------
   def get_bot_id
     response = @access_token.get(
-      '/account/verify_credentials.json'
-    )
+                                 '/account/verify_credentials.json'
+                                 )
     
     mystatus = JSON.parse( response.body )
     @bot_id = mystatus["screen_name"]
-
+    
   end
-
+  
   #--------- Botへのリプライか真偽を取得  ---------
   def is_reply( tw )
     return self.search_tweet( @bot_id, tw )
@@ -43,50 +45,43 @@ class MyTwitterBot < TwitterBot
     tweets = self.get_tweet
     
     tweets.each do |post|
-      if self.is_reply(post) then
-        if self.search_tweet("｣と言って", post) then
-          head = post["message"].index("｢") + 1
-          tail = post["message"].rindex("｣") - 1
-          self.tweet( post["message"].slice(head..tail))
+      if self.is_reply(post) 
+        if (/｢(.+)｣と言って/ =~ post["message"]) != nil  
+          self.tweet( $1 )
         end
-
-      end
-
-    end
-
-  end
-
-  #--------- 誕生日の人がいれば@付きつぶやきを飛ばす ---------
-  def notice_birth
-    birthday_data = YAML.load_file('./birthday.yml')
-
-    today = Time.now
-
-    birthday_data.each do |prof|
-      if prof["month"] == today.month then
-        if prof["day"] == today.day then
-          str = "今日は" 
-          str << prof["name"] 
-          str << " @" 
-          str << prof["ID"] 
-          str << " さんの誕生日です！みんなでお祝いしましょう！" 
-          self.tweet( str )
-        end
-
+        
       end
       
     end
-
+    
   end
-
   
-
+  #--------- 誕生日の人がいれば@付きつぶやきを飛ばす ---------
+  def notice_birth
+    birthday_data = YAML.load_file('./birthday.yml')
+    
+    today = Time.now
+ 
+    birthday_data.each do |prof|
+      if prof["month"] == today.month 
+        if prof["day"] == today.day
+          str = "今日は" + prof["name"] + " @" + prof["ID"] \
+          +  " さんの誕生日です！みんなでお祝いしましょう！" 
+          self.tweet( str )
+        end
+        
+      end
+      
+    end
+    
+  end
+  
 end
 
 #MyTwitterBotの生成
 print "Start MyTwitterBot.\n"
 mbot = MyTwitterBot.new()
 #mbot.answer_say
-#mbot.notice_birth
+mbot.notice_birth
 print "End MyTwitterBot.\n"
 
